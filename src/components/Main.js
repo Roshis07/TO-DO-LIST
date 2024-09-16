a739861ccfdbbd863aab4d6c809d76cdf0d688fc
Roshis <roshis.tamang@gmail.com>
2024-09-16 20:39:34 +1000

    form created with onSubmit

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,23 @@
 import { Box, FormControl, Typography } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
 
 const Main = () => {
+  const [data, setData] = useState([]);
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const newData = {
+      name: event.target.elements["name"].value,
+      hours: event.target.elements["hours"].value,
+    };
+    const combinedData = (prevDatas) => {
+      return [...prevDatas, newData];
+    };
+    setData(combinedData);
+    event.target.reset();
+  };
+  console.log(data);
   return (
     <>
       <Typography variant="h2" color="black">
@@ -10,10 +26,15 @@ const Main = () => {
       </Typography>
 
       <Box sx={{ p: 2, border: 1, width: "800px", margin: "0 auto" }}>
-        <FormControl>
-          <input />
-          <button>Submit</button>
-        </FormControl>
+        <form onSubmit={submitHandler}>
+          <FormControl>
+            <label>Please put your task name</label>
+            <input name="name" />
+            <label>Please put your hours</label>
+            <input name="hours" />
+            <input type="submit" />
+          </FormControl>
+        </form>
       </Box>
     </>
   );
